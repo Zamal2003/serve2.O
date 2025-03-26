e1622cbeeaa515754e1c5fbaab1af49b61a81acd
mdaman9939 <mdjamaluddina007@gmail.com>
2025-03-26 14:44:44 +0530

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require('dotenv').config();
+const app = express();
 
 app.use(
   cors({
@@ -16,7 +17,6 @@ app.use(
 const authRoutes = require("./routes/authRoutes");
 const dashBoard= require("./routes/dashBoard")
 const Observation= require("./routes/observations")
-const app = express();
 // const PORT = 5000;
 const PORT = process.env.PORT || 5000;
 
